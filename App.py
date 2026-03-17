fa2f9e7f50a6a1580f6a1e3ef3f16ff457f59811
rp2023 <122508152+rp2023@users.noreply.github.com>
2026-03-17 20:19:39 +0530

    Update App.py

--- App.py
+++ App.py
@@ -209,4 +209,4 @@ def export_json():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000
+    app.run(debug=True, host='0.0.0.0', port=5000)
